81966a974fb68e9e0e059a18754e89cd4eecfdf1
Fabian C Galasel <FG296@live.mdx.ac.uk>
2024-04-17 23:56:11 +0000

    Updated product_item.h

--- product_item.h
+++ product_item.h
@@ -3,6 +3,7 @@
 #define PRODUCT_ITEM_H
 
 #include <string>
+#include <vector>
 
 class product_item {
 private:
@@ -10,22 +11,26 @@ private:
     std::string name;       // Product name
     std::string description;    // Product description
     int stockQuantity;      // Stock quantity
+    double price;
 
 public:
     product_item(); // Default constructor
-    product_item(int productID, std::string name, std::string description, int stockQuantity);
+    product_item(int productID, std::string name, std::string description, int stockQuantity, double price);
 
     // Getter functions
     int getProductID() const { return productID; } // Retrieve product ID
     std::string getName() const { return name; } // Retrieve product name
     std::string getDescription() const { return description; } // Retrieve product description
     int getStockQuantity() const { return stockQuantity; } // Retrieve stock quantity
+    double getPrice() const { return price; }
 
     // Setter functions
     void setProductID(int id) { productID = id; } // Set product ID
     void setName(const std::string& n) { name = n; } // Set product name
     void setDescription(const std::string& desc) { description = desc; } // Set product description
     void setStockQuantity(int quantity) { stockQuantity = quantity; } // Set stock quantity
+    void setPrice(double price) { this->price = price; }
+
 
     // Additional member functions for managing stock
     void updateStock(int newQuantity);     // Update stock quantity

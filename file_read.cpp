86b45ca3702c115becbdc9b00d516f3e0348858f
Fabian C Galasel <FG296@live.mdx.ac.uk>
2024-04-17 23:54:08 +0000

    Updated file_read.cpp

--- file_read.cpp
+++ file_read.cpp
@@ -1,60 +1,37 @@
+#include "file_read.h"
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <string>
-
-const int MAX_GAMES = 20; // Maximum number of games to read
-
-// Struct to represent a game
-struct Game {
-    std::string name;
-    int stockQuantity;
-    double price;
-};
 
 // Function to read games from a TXT file
-bool readGamesFromTXT(const std::string &filename, Game games[], int &numGames) {
+std::vector<Game> readGamesFromTXT(const std::string& filename) {
     std::ifstream file(filename);
+    std::vector<Game> games;
+
     if (file.is_open()) {
-        int count = 0; // Variable to count the number of games read
         std::string line;
 
-        // Discard the header line
+        // Read and discard the header line
         std::getline(file, line);
 
-        while (file.good() && numGames < MAX_GAMES &&
-               std::getline(file, line)) {
+        while (std::getline(file, line)) {
             std::stringstream ss(line);
 
             std::string gameName;
             int stockQuantity;
             double price;
+            char comma;
 
-            char comma; // to read and discard the commas
             std::getline(ss, gameName, ',');
             ss >> stockQuantity >> comma >> price;
 
-            // Debug output
-            std::cout << "Read from file: " << gameName << ", Stock: " << stockQuantity << ", Price: £" << price << std::endl;
-
-            // Store the game in the array
-            games[numGames++] = {gameName, stockQuantity, price};
-
-            count++;
+            games.push_back({gameName, stockQuantity, price});
         }
+
         file.close();
-        std::cout << "File successfully opened: " << filename << std::endl;
-        return true;
     } else {
         std::cout << "Error opening file: " << filename << std::endl;
-        return false;
     }
-}
 
-std::string getUserInputFilename() {
-    std::string filename;
-    std::cout << "Enter the filename: ";
-    std::cin >> filename;
-    return filename;
+    return games;
 }
-

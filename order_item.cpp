24ce71379382694a07b1adf16d3fc58e2facf417
Ovie Golde <OG148@live.mdx.ac.uk>
2024-04-17 05:24:12 +0000

    Updated order_item.cpp

--- order_item.cpp
+++ order_item.cpp
@@ -1,14 +1,13 @@
-// order_item.cpp
 #include "order_item.h"
 
-order_item::order_item(int order_id, int customer_id, int product_id, int quantity, std::string status)
-        : order_id(order_id), customer_id(customer_id), product_id(product_id), quantity(quantity), status(status) {}
+order_item::order_item(int order_id, int customer_id, std::string product, std::string status)
+        : order_id(order_id), customer_id(customer_id), product(product), status(status) {}
 
 void order_item::update_status(std::string new_status) {
     status = new_status;
 }
 
-std::string order_item::track_order_status() {
+std::string order_item::track_order_status() const {
     return status;
 }
 
@@ -16,3 +15,6 @@ int order_item::getOrderID() const {
     return order_id;
 }
 
+std::string order_item::getProduct() const {
+    return product;
+}
